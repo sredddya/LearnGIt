97389636498e956edcc5e8e58e5078154b8953f6
Saikiran <saikiranreddyxxx@gmail.com>
2024-11-23 18:58:19 -0500

    added day 1 activity

---
+++ cftl.py
@@ -0,0 +1,15 @@
+'''#Day 1 Activity
+print("I Love Python")
+#Activity 2
+print("Learning with 'TOTAL Python' is super fun!")
+#Activity 3
+print("555")'''
+
+
+
+# Ask the user for two inputs
+input1 = input("Whats your fav beer: ")
+input2 = input("whats your fav color: ")
+
+# Print both inputs in one statementca
+print(f"You entered: First input: {input1}, Second input: {input2}")
