975da56d2869587572714c5f13d3104ada75ac1c
Saikiran <saikiranreddyxxx@gmail.com>
2025-03-11 15:06:24 -0400

    excercise2

--- printstatement.py
+++ printstatement.py
@@ -2,3 +2,13 @@ A=input("enter the name you like: ")
 B=input("enter the colour you like")
 
 print(f"the name of my company is {A} {B}")
+
+#Excercise=2
+
+Employee_Name=input("Enter the Name of the Employee: ")
+sales=int(input("enter amount of sales done by employee: "))
+commision=sales*13/100
+
+
+print(f"the employee {Employee_Name} sold {sales} for that he received the commision of {commision}")
+
